0b2e87ae7c07c4e60c2ddbd4ce6527f87ccacec8
Gaurav Shrivastava <gauravsanu@gmail.com>
2024-02-11 14:07:15 +0530

    With batch file

---
+++ run.bat
@@ -0,0 +1,12 @@
+@ECHO ON
+IF "%1"=="start" (
+    ECHO start MyWebApp
+    start "MyWebApp" mvnw spring-boot:run
+) ELSE IF "%1"=="stop" (
+    ECHO stop MyWebApp
+    #TASKKILL /FI "WINDOWTITLE eq MyWebApp"
+    TASKKILL /FI "WINDOWTITLE eq mvnw"
+) ELSE (
+    ECHO please, use "run.bat start" or "run.bat stop"
+)
+pause
